4fd207160fd18f5cedbac23d9897c1075f3a34cb
Pecz <p.balint1000@gmail.com>
2022-10-17 02:41:39 +0200

    done..

--- slider.js
+++ slider.js
@@ -1,15 +1,26 @@
-var counter = 1;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-document.getElementById('elore').onclick = function() {
-    console.log("sajt");
-};
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 4){
-       counter = 1;
-    }
-}, 5000);
+function buttonSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
